72393698a1cbac6720e07ce863bdf720a07f05e7
Greg Anderson <greg@infobytes.com>
2015-02-27 15:29:00 -0700

    Server now works on the network
    Server now prints out it's network IP and the client now asks for the server IP.

--- client.py
+++ client.py
@@ -1,9 +1,11 @@
 import requests
 
+ip = raw_input('Server IP (e.g. 192.168.1.17): ')
+
 try:
 	while True:
 		message = raw_input('Message: ')
-		r = requests.get('http://localhost:5000/' + message)
+		r = requests.get(ip + ':5000/' + message)
 		print r.text
 except:
 	print
--- server.py
+++ server.py
@@ -1,15 +1,27 @@
 from flask import Flask
+import commands
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-	return "Hello, World!"
+	return "Hello world!"
 
 @app.route('/<user_message>', methods=['GET'])
 def message(user_message):
 	print user_message
 	return user_message
 
+def get_network_ip():
+	status, output = commands.getstatusoutput('ifconfig')
+
+	o = output.split('\n')
+
+	for line in o:
+		if '192.168' in line:
+			start = line[line.find('192.168'):]
+			return start[:start.find(' ')]
+
 if __name__ == '__main__':
-	app.run(debug=True)
+	print 'Network IP: ' + get_network_ip()
+	app.run(host='0.0.0.0')
